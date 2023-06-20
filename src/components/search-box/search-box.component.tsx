1fc2f46f451e9161c5c9dc47a5f11a108eb1a87f
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-20 16:16:38 -0400

    Conv search-box to ts

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,15 +1,56 @@
-import { Component } from 'react';
+import { Component, ChangeEventHandler } from 'react';
 import './search-box.styles.css';
 
-const name: string = '1234561';
+// const name: string = '1234561';
 
-const SearchBox = ({ className, placeholder, onChangeHandler }) => {
+// const func: (a: string, b: number, c: boolean) => void = (a, b, c) => {}
+
+// interface ISearchBoxProps {
+//     className: string;
+//     placeholder: string;
+// }
+
+// interface ISearchBoxProps {
+//     onChangeHandler: (a: string) => void;
+//     placeholder: string;
+// }
+
+type SearchBoxProps = {
+    className: string,
+    placeholder?: string;
+    onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+}
+
+// type CanadianAddress = {
+//     street: string;
+//     province: string;
+// }
+
+// type USAddress = {
+//     street: string;
+//     state: string;
+// }
+
+// type ItalianAddress = {
+//     street: string;
+//     region: string;
+// }
+
+// type Address = CanadianAddress | USAddress | ItalianAddress;
+
+// const Address: Address = {
+//     street: "",
+//     state: "",
+// }
+
+const SearchBox = ({ className, placeholder, onChangeHandler, }: SearchBoxProps) => {
     return (
-        <input className={`search-box ${className}`} 
+        <input 
+        className={`search-box ${className}`} 
         type='search' 
         placeholder={placeholder} 
         onChange={onChangeHandler}/>
     );
-}
+};
 
 export default SearchBox;

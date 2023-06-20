84ca2657e17cf4c419ba4621061c528c6a3284e3
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-20 15:53:11 -0400

    Adding typescript

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import './search-box.styles.css';
 
+const name: string = '1234561';
+
 const SearchBox = ({ className, placeholder, onChangeHandler }) => {
     return (
         <input className={`search-box ${className}`} 
